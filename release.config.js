42ac8675370657bb15e56d5bed41ec3ef34f2677
Alston Dias <adias@redhat.com>
2023-05-07 15:18:30 +0000

    Reverted all changes

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: "workflow",
+    branches: "master",
     repositoryUrl: "https://github.com/AlstonDias/github-action-ci-cd",
     plugins: ["@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
