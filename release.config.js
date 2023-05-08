e10e438e82f1550e251f4259fac39e69050130c0
Alston Dias <adias@redhat.com>
2023-05-08 09:49:00 +0000

    fix(release.config.js): updated

--- release.config.js
+++ release.config.js
@@ -1,13 +1,11 @@
 module.exports = {
     branches: "master",
-    repositoryUrl: "https://github.com/AlstonDias/github-action-ci-cd",
-    plugins: ["@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        ["@semantic-release/github",{
-            assets: [
-                {path: "build.zip", label: "Build"},
-                {path: "coverage.zip", label: "Coverage"}
-            ]
-        }]
+    repositoryUrl: "https://github.com/AlstonDias/github-action-ci-cd/",
+    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', ['@semantic-release/github', {
+        assets: [
+            { path: "build.zip", label: "Build" },
+            { path: "coverage.zip", label: "Coverage" }
+        ]
+    }]
     ]
 }
